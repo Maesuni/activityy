3c84d9a1a28348b05e9fc99ebb53bdd5cfa6a8ae
Mae <146410195+Maesuni@users.noreply.github.com>
2024-10-29 11:01:16 +0800

    mars.php

---
+++ mars.php
@@ -0,0 +1,7 @@
+<?php
+// mars.php
+echo "Planet: Mars\n";
+echo "Distance from the Sun: 227.9 million km\n";
+echo "Diameter: 6,779 km\n";
+echo "Atmosphere: Thin atmosphere mostly made of carbon dioxide, with nitrogen and argon.\n";
+?>
