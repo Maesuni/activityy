d8efa8588abd7d1ab687bf357a978baf2f447958
Mae <146410195+Maesuni@users.noreply.github.com>
2024-10-29 11:00:26 +0800

    venus.php

---
+++ venus.php
@@ -0,0 +1,7 @@
+<?php
+// venus.php
+echo "Planet: Venus\n";
+echo "Distance from the Sun: 108.2 million km\n";
+echo "Diameter: 12,104 km\n";
+echo "Atmosphere: Thick atmosphere mostly made of carbon dioxide with clouds of sulfuric acid.\n";
+?>
