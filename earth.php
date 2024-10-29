639ada45733cb31b4a66ddb80e564d5519d6fe2e
Mae <146410195+Maesuni@users.noreply.github.com>
2024-10-29 11:00:52 +0800

    earth.php

---
+++ earth.php
@@ -0,0 +1,7 @@
+<?php
+// earth.php
+echo "Planet: Earth\n";
+echo "Distance from the Sun: 149.6 million km\n";
+echo "Diameter: 12,742 km\n";
+echo "Atmosphere: Nitrogen, Oxygen, Argon, and trace gases.\n";
+?>
